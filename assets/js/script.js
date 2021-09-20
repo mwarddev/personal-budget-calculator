42bffe8970699f0c2afd4296fe7ba199f23c2a21
mwarddev <martinward1981@googlemail.com>
2021-09-20 00:55:06 +0000

    Add event listeners to all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-//Add event listener and functionality for the currency select option.
+// Wait for DOM to finish loading before running JS
+// Add event listeners to all buttons
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                calculate();
+            } else {
+                addBox();
+            }
+        })
+    }
+})
+
+// Add event listener and functionality for the currency select option.
 
 let selectedCurrency = document.getElementById("currency");
 selectedCurrency.addEventListener("click", function() {
