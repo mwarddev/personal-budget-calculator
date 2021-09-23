e9c6712201a6015c51ff7021997a9f4201b3b61a
mwarddev <martinward1981@googlemail.com>
2021-09-23 22:36:29 +0000

    Add summing loops for each section to populate chart data

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,9 @@ selectedCurrency.addEventListener("click", function() {
 // }
 
 
-
+/**
+ * Calculates the total income
+ */
 function calculateIncome() {
     let userIncome = document.getElementsByClassName("income");
     let totalIncome = 0;
@@ -68,6 +70,9 @@ function calculateIncome() {
     return totalIncome;
 }
 
+/**
+ * calculates the total expenditure
+ */
 function calculateExpenses() {
     let userExpense = document.getElementsByClassName('expense');
     let totalExpense = 0;
@@ -84,30 +89,32 @@ function calculateExpenses() {
     return totalExpense;
 }
 
+/**
+ * Calculates the result of total income minus total expenditure and outputs data to the DOM
+ */
 function calculateResult() {
 
     
-    let outcome = parseFloat(calculateIncome() - calculateExpenses());
+    let outcome = calculateIncome() - calculateExpenses();
     let resHtml = 
     `
     <div id="incomeTotal">
         <h3>Total Income</h3>
-        <p>${selectedCurrency.value}${Math.round(parseFloat(calculateIncome())* 100) / 100}</p>
+        <p>${selectedCurrency.value}${Math.round(calculateIncome()* 100) / 100}</p>
     </div>
     <div id="expenseTotal">
         <h3>Total Expenditure</h3>
-        <p>${selectedCurrency.value}${Math.round(parseFloat(calculateExpenses())* 100) / 100}</p>
+        <p>${selectedCurrency.value}${Math.round(calculateExpenses()* 100) / 100}</p>
     </div>
     <div id="outcome">
         <h3>Total Remaining</h3>
-        <p>${selectedCurrency.value}${Math.round(parseFloat(outcome)* 100) / 100}</p>
-    </div>
-    <div id="chart">
+        <p>${selectedCurrency.value}${Math.round(outcome* 100) / 100}</p>
     </div>
     `;
+
     document.getElementById('results').innerHTML = resHtml;
        
-// Style the outcomes depending on outcomes
+    // Style the outcomes using conditional statements
     if(calculateIncome() > 0) {
         document.getElementById('incomeTotal').style.color = "#00b200";
     } else {
@@ -120,11 +127,113 @@ function calculateResult() {
         document.getElementById('outcome').style.color = "#ff0000";
     }
 
-// Add chart to results section
+    // Calculate sum of section data for pie chart
+    let financeAndDebt = document.getElementsByClassName("finances");
+    let financeAndDebtSum = 0;
 
+    for(let finance of financeAndDebt) {
+        
+        if(finance.value) {
+            financeAndDebtSum += parseFloat(finance.value);
+        } else {
+            financeAndDebtSum += 0;
+        }
+        return financeAndDebtSum;
+    }
+
+    let savingsSection = document.getElementsByClassName("savings");
+    let savingsSum = 0;
+    
+    for(let saving of savingsSection) {
+            
+        if(saving.value) {
+            savingsSum += parseFloat(saving.value);
+        } else {
+            savingsSum += 0;
+        }
+        return savingsSum;
+    }
+
+    let billsSection = document.getElementsByClassName("bills");
+    let billsSum = 0;
+    
+    for(let bill of billsSection) {
+            
+        if(bill.value) {
+            billsSum += parseFloat(bill.value);
+        } else {
+            billsSum += 0;
+        }
+        return billsSum;
+    }
+
+    let insuranceSection = document.getElementsByClassName("insurance");
+    let insuranceSum = 0;
+    
+    for(let insure of insuranceSection) {
+            
+        if(insure.value) {
+            insuranceSum += parseFloat(insure.value);
+        } else {
+            insuranceSum += 0;
+        }
+        return insuranceSum;
+    }
+
+    let subAndDdSection = document.getElementsByClassName("subscriptions");
+    let subAndDdSum = 0;
+    
+    for(let sub of subAndDdSection) {
+            
+        if(sub.value) {
+            subAndDdSum += parseFloat(sub.value);
+        } else {
+            subAndDdSum += 0;
+        }
+        return subAndDdSum;
+    }
+
+    let livingSection = document.getElementsByClassName("living");
+    let livingSum = 0;
+    
+    for(let live of livingSection) {
+            
+        if(live.value) {
+            livingSum += parseFloat(live.value);
+        } else {
+            livingSum += 0;
+        }
+        return livingSum;
+    }
+
+    let familySection = document.getElementsByClassName("family");
+    let familySum = 0;
+    
+    for(let fam of familySection) {
+            
+        if(fam.value) {
+            familySum += parseFloat(fam.value);
+        } else {
+            familySum += 0;
+        }
+        return familySum;
+    }
+
+    let leisureSection = document.getElementsByClassName("leisure");
+    let leisureSum = 0;
+    
+    for(let l of leisureSection) {
+            
+        if(l.value) {
+            leisureSum += parseFloat(l.value);
+        } else {
+            leisureSum += 0;
+        }
+        return leisureSum;
+    }
 }
+
 
 function addBox() {
 
 }
-
